9c152424af6429cb07dfaf7e4d595c67c1ef0263
habales <falko.lehmann@habales.de>
2015-12-06 10:17:56 +0100

    First working Version
    Config for configuration json is in place.
    Json is read and navigation is working

--- mopidy_easywebradio/__init__.py
+++ mopidy_easywebradio/__init__.py
@@ -2,21 +2,12 @@ from __future__ import unicode_literals
 
 import logging
 import os
-
-# TODO: Remove entirely if you don't register GStreamer elements below
 import pygst
 pygst.require('0.10')
-import gst
-import gobject
-
 from mopidy import config, ext
 
-
 __version__ = '0.1.0'
-
-# TODO: If you need to log, use loggers named after the current Python module
 logger = logging.getLogger(__name__)
-
 
 class Extension(ext.Extension):
 
@@ -30,13 +21,10 @@ class Extension(ext.Extension):
 
     def get_config_schema(self):
         schema = super(Extension, self).get_config_schema()
-        # TODO: Comment in and edit, or remove entirely
-        #schema['username'] = config.String()
-        #schema['password'] = config.Secret()
+        schema['config_file'] = config.Path()
         return schema
 
     def setup(self, registry):
         from .actor import EasyRadioBackend
         registry.add('backend', EasyRadioBackend)
 
-	pass
--- mopidy_easywebradio/actor.py
+++ mopidy_easywebradio/actor.py
@@ -8,34 +8,43 @@ import os, json
 
 import pykka
 
-
-
 logger = logging.getLogger(__name__)
 
 
 class EasyRadioBackend(pykka.ThreadingActor, backend.Backend):
-    uri_schemes = ['easyradio']
+
+    uri_schemes = ['easywebradio']
 
     def __init__(self, config, audio):
         super(EasyRadioBackend, self).__init__()
-
-        self.library = EasyRadioLibrary(self)
+        webradio_json = config['easywebradio']['config_file']
+        self.library = EasyRadioLibrary(self,webradio_json )
 
 
 class EasyRadioLibrary(backend.LibraryProvider):
-    root_directory = Ref.directory(uri='easyradio:root', name='Easy Radio')
 
-    def __init__(self, backend):
+    root_directory = Ref.directory(uri='easywebradio:root', name='Easy Radio')
+
+    def __init__(self, backend, webradio_json):
         super(EasyRadioLibrary, self).__init__(backend)
+        self.webradio_json = webradio_json
 
     def browse(self, uri):
 
-        logger.info('Browsing %s' % uri)
-        radio_file  = os.path.join(os.path.dirname(__file__), 'sample_list.json')
+        radio_file = os.path.join(os.path.dirname(__file__), self.webradio_json)
         with open(radio_file) as data_file:
             data = json.load(data_file)
 
         result = []
+        if "easywebradio:root" == uri:
+            for cat in data:
+                catref = Ref.directory(uri='easywebradio:category:/' +cat['category']['cat_name'], name=cat['category']['cat_name'])
+                result.append(catref)
+        else:
+            for cat in data:
+                if cat['category']['cat_name'] in uri:
+                    for pls in cat['category']['stations']:
+                        result.append(Ref.track(uri=pls['streamurl'], name=pls['name']))
 
         return result
 
